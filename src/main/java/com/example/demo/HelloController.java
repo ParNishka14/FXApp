2ed8d825739a8d93cdf7129b3521b267a3dd27ee
ParNishka14 <48641303+ParNishka14@users.noreply.github.com>
2023-10-20 13:14:27 +0300

    add menu

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
@@ -22,6 +25,12 @@ public class HelloController {
         if(db.checkPassword(loginText.getText(), passwordText.getText()) !=null ){
             System.out.println("yes");
             errorLoginText.setVisible(false);
+            Stage menuStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("newFX.fxml"));
+            Scene scene = new Scene(loader.load(), 320, 240);
+            menuStage.setTitle("Hello!");
+            menuStage.setScene(scene);
+            menuStage.show();
         } else {
             errorLoginText.setVisible(true);
         }
---
+++ src/main/java/com/example/demo/MenuController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController{
+
+    public void start(Stage stage) throws IOException {
+
+    }
+}
