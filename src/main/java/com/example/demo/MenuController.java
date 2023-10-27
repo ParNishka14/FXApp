a16d0a0319391cca968563462366d2e2d7383b09
ParNishka14 <48641303+ParNishka14@users.noreply.github.com>
2023-10-27 12:31:55 +0300

    add menu

--- src/main/java/com/example/demo/MenuController.java
+++ src/main/java/com/example/demo/MenuController.java
@@ -1,14 +1,32 @@
 package com.example.demo;
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class MenuController{
+public class MenuController  {
 
-    public void start(Stage stage) throws IOException {
+    @FXML
+    public VBox itemsBox = new VBox();
+    public Stage newStage;
+
+
+        private ArrayList<PizzaItem> items = new ArrayList<>();
+    {
+        items.add(new PizzaItem(10,"Peperony"));
+        items.add(new PizzaItem(15,"Margaritta"));
+        items.add(new PizzaItem(150,"idk"));
+    }
+
+    public void initItems() throws IOException {
 
     }
+
 }
---
+++ src/main/java/com/example/demo/PizzaItem.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class PizzaItem {
+    public int price;
+    public String name;
+
+    public PizzaItem(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
